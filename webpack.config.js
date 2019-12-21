e1266fc48d756235628c530637746dbc1ea7283c
dakyung ko <kdkyung57@gmail.com>
2019-12-21 15:43:19 +0900

    add manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
   entry: './src/index.js',
@@ -21,6 +22,20 @@ module.exports = {
   plugins: [
     new htmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new webpackPwaManifest({
+      name: 'react memo app',
+      short_name: 'memo',
+      description: '리액트로 제작한 메모앱',
+      background_color: '#ffffff',
+      crossorigin: 'use-credentials',
+      theme_color: '#eeeeee',
+      icons: [
+        {
+          src: path.resolve('src/assets/Icon.png'),
+          sizes: [96, 128, 192, 256, 384, 512]
+        }
+      ]
     })
   ]
 };
